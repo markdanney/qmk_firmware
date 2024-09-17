45ddb40f5d620a5ca4ae8684d08303afae101f3f
Mark-Danney Oonk <mark-danney.oonk@essent.nl>
2024-09-17 13:11:51 +0200

    add lily keymap

---
+++ keyboards/lily58/keymaps/markdanney/config.h
@@ -0,0 +1,39 @@
+/*
+This is the c configuration file for the keymap
+
+Copyright 2012 Jun Wako <wakojun@gmail.com>
+Copyright 2015 Jack Humbert
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 2 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#pragma once
+
+// #define QUICK_TAP_TERM 0
+// #define TAPPING_TERM 100
+
+#undef RGBLIGHT_LED_COUNT
+
+#define BOTH_SHIFTS_TURNS_ON_CAPS_WORD
+
+#define SPLIT_LED_STATE_ENABLE
+
+#undef LOCKING_SUPPORT_ENABLE
+#undef LOCKING_RESYNC_ENABLE
+#define NO_ACTION_ONESHOT
+#define NO_MUSIC_MODE
+#define NO_ACTION_TAPPING
+#define LAYER_STATE_8BIT
+#define NO_DEBUG
+#define NO_PRINT
