1f26f77dfc030d51c51ac401d2617c8b2d523f92
Mark-Danney Oonk <mark-danney.oonk@essent.nl>
2024-09-17 20:15:35 +0200

    fix build error

--- keyboards/lily58/keymaps/markdanney/config.h
+++ keyboards/lily58/keymaps/markdanney/config.h
@@ -35,5 +35,3 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define NO_MUSIC_MODE
 #define NO_ACTION_TAPPING
 #define LAYER_STATE_8BIT
-#define NO_DEBUG
-#define NO_PRINT
